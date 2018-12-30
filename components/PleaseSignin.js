2835e0a859855fc3ecaa273f98f8c75dbb677f61
Steven Phan <ptviet@live.com>
2018-12-30 11:53:01 +1100

    add secure route

---
+++ components/PleaseSignin.js
@@ -0,0 +1,26 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import SignIn from './SignIn';
+import ErrorMessage from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
+
+const PleaseSignin = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, error, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <ErrorMessage error={error} />;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please login first.</p>
+            <SignIn />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
+
+export default PleaseSignin;
