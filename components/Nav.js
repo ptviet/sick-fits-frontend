3762957359b85ea99a26157bda2e9f51b3279505
Steven Phan <ptviet@live.com>
2019-01-07 22:04:31 +1100

    add Edit Permission Nav link

--- components/Nav.js
+++ components/Nav.js
@@ -8,45 +8,59 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 
 const Nav = () => (
   <User>
-    {({ data: { me } }) => (
-      <NavStyles>
-        <Link href="/items">
-          <a>Shop</a>
-        </Link>
-        {me && (
-          <>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => (
-                <button onClick={toggleCart}>
-                  My Cart
-                  <CartCount
-                    count={me.cart.reduce(
-                      (total, cartItem) => total + cartItem.quantity,
-                      0
-                    )}
-                  />
-                </button>
-              )}
-            </Mutation>
-            <SignOut />
-          </>
-        )}
-        {!me && (
-          <Link href="/signup">
-            <a>Sign Up - Sign In</a>
+    {({ data: { me } }) => {
+      let matchedPermissions;
+      if (me) {
+        matchedPermissions = me.permissions.filter(myPermission =>
+          ['PERMISSIONUPDATE', 'ADMIN'].includes(myPermission)
+        );
+      }
+
+      return (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
           </Link>
-        )}
-      </NavStyles>
-    )}
+          {me && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              {matchedPermissions.length && (
+                <Link href="/permission">
+                  <a>Permissions</a>
+                </Link>
+              )}
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => (
+                  <button onClick={toggleCart}>
+                    My Cart
+                    <CartCount
+                      count={me.cart.reduce(
+                        (total, cartItem) => total + cartItem.quantity,
+                        0
+                      )}
+                    />
+                  </button>
+                )}
+              </Mutation>
+              <SignOut />
+            </>
+          )}
+          {!me && (
+            <Link href="/signup">
+              <a>Sign Up - Sign In</a>
+            </Link>
+          )}
+        </NavStyles>
+      );
+    }}
   </User>
 );
 
