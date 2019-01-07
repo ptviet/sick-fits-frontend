fde765fffb22d8116ec060e288bc28efd8b643f3
Steven Phan <ptviet@live.com>
2019-01-07 16:40:59 +1100

    edit config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const { endpointProd } = process.env;
+export const endpointProd = process.env.endpointProd;
 export const perPage = 4;
-export const { cloudinary } = process.env;
-export const { stripePublishableKey } = process.env;
+export const cloudinary = process.env.cloudinary;
+export const stripePublishableKey = process.env.stripePublishableKey;
 export const paymentIcon = `/static/paymentIcon.png`;
