062ce473b3462d32b67d50eecef721ebd80dd7ec
Steven Phan <ptviet@live.com>
2019-01-07 17:48:37 +1100

    test endpoint

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const endpointProd = process.env.endpointProd;
+export const endpointProd = process.env.ENDPOINT_PROD;
 export const perPage = 4;
-export const cloudinary = process.env.cloudinary;
-export const stripePublishableKey = process.env.stripePublishableKey;
+export const cloudinary = process.env.CLOUDINARY;
+export const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
 export const paymentIcon = `/static/paymentIcon.png`;
