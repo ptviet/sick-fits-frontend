e4307b3ae84b6f99e8b1db49992d587f685f3c2d
Steven Phan <ptviet@live.com>
2019-01-07 16:17:12 +1100

    change config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const { endpointProd } = process.env;
 export const perPage = 4;
-export const cloudinary = `https://api.cloudinary.com/v1_1/ptviet/image/upload`;
-export const stripePublishableKey = `pk_test_emvOb4Bkdi7ctlbf5B9drlJZ`;
+export const { cloudinary } = process.env;
+export const { stripePublishableKey } = process.env;
 export const paymentIcon = `/static/paymentIcon.png`;
