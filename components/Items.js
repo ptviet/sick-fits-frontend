1d88d492bd24f332a7d9f161ab69fed3bfa12c2b
Steven Phan <ptviet@live.com>
2019-01-08 18:45:15 +1100

    edit Items columns size

--- components/Items.js
+++ components/Items.js
@@ -26,7 +26,7 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
   grid-gap: 20px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
