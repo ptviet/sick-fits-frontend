a0866fb015e3eac0c80060202ce9fbc1e37ee446
Steven Phan <ptviet@live.com>
2019-01-08 13:31:24 +1100

    fix redirect after signin/signup

---
+++ components/Auth.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Router from 'next/router';
+import styled from 'styled-components';
+import User, { CURRENT_USER_QUERY } from './User';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import RequestResetPassword from './RequestResetPassword';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+class Auth extends Component {
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => {
+          if (me)
+            Router.push({
+              pathname: '/'
+            });
+          return (
+            <Columns>
+              <SignUp />
+              <SignIn />
+              <RequestResetPassword />
+            </Columns>
+          );
+        }}
+      </User>
+    );
+  }
+}
+
+export default Auth;
--- pages/signup.js
+++ pages/signup.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
-import SignUp from '../components/SignUp';
-import SignIn from '../components/SignIn';
-import RequestResetPassword from '../components/RequestResetPassword';
+// import SignUp from '../components/SignUp';
+// import SignIn from '../components/SignIn';
+// import RequestResetPassword from '../components/RequestResetPassword';
+import Auth from '../components/Auth';
 
-const Columns = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 20px;
-`;
+// const Columns = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+//   grid-gap: 20px;
+// `;
 
 const SignupPage = props => (
-  <Columns>
-    <SignUp />
-    <SignIn />
-    <RequestResetPassword />
-  </Columns>
+  <Auth />
+  // <Columns>
+  //   <SignUp />
+  //   <SignIn />
+  //   <RequestResetPassword />
+  // </Columns>
 );
 
 export default SignupPage;
